7684d43efb5f669301abf5f7aa6367e9f9413f1a
Mahak2357 <mahak09822@gmail.com>
2026-09-22 19:25:43 +0530

    added branches

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1>Github Actions</h1>
+      <h1>Github Actions(ci/cd)</h1>
       <h2>Learning GitHub Actions by building a simple app</h2>
     </>
   )
